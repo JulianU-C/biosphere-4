d368bb215ca89d4a17d365c8a9e4f840ea541448
Julian Urrutia-Carter <128261117+JulianU-C@users.noreply.github.com>
2026-03-20 22:41:57 -0400

    Update and rename biosphere_IV_3-10.R to bios4-March10.R

--- March10/bios4-March10.R
+++ March10/bios4-March10.R
@@ -6,13 +6,13 @@
 
 # 1/2 plate setup:
   # col1 = Water
-  # col2 = E. coli (MClover)
-  # col3 = J. liv (dusy basin)
-  # col4 = Agrobacterium UM1803
+  # col2 = E. coli
+  # col3 = J. lividum
+  # col4 = Agrobacterium sp.
   # col5 = Enterococcus sp.
   # col6 = Media
-  # B-D = No STX
-  # E-G = STX
+  # B-D = No Toxin
+  # E-G = Toxin
   # A and H = Empty wells
 
 # this is the only library we'll need, don't get stuck in library hell
@@ -52,7 +52,7 @@ processPlate <- function(file_path) {
                          column == 4 ~ "Agrobacterium UM1803",
                          column == 5 ~ "Enterococcus sp.",
                          column == 6 ~ "Media"),
-      toxin = case_when(row %in% c("E", "F", "G") ~ "STX", TRUE ~ "SHAM"),
+      toxin = case_when(row %in% c("E", "F", "G") ~ "Toxin", TRUE ~ "SHAM"),
       file = basename(file_path),
       day = str_extract(file, "Day\\d+"),
       dayNum = str_extract_all(day, "\\d"),
@@ -123,93 +123,3 @@ ggsave("AdvLine.png", plot = AdvLine,
        height = 5, 
        units = c("in"),
        dpi = 300)
-
-## Count Data: Differential expression==========================================
-## =============================================================================
-library(tximport)
-library(readr)
-library(DESeq2)
-library(tidyverse)
-library(EnhancedVolcano)
-
-## load metadata
-## -----------------------------------------------------------------------------
-# NOTE: ok I loaded my metadata as a tibble before (read_csv) and it wouldn't
-# subset in dds?? Not sure why but make sure use read.csv for this
-md <- read.csv("meta.csv")
-
-# set "no" treatment and "neg" infection as reference levels
-md$tissue <- factor(md$tissue, levels = c("skin", "spleen"))
-# Default is alphabetical order, so can just leave alone and skin will be ref.
-
-## load read counts
-## -----------------------------------------------------------------------------
-countsTableRaw <- read.delim("gene_counts.txt", 
-                             comment.char = "#",
-                             row.names = 1)
-
-counts <- countsTableRaw %>% dplyr::select(!(Chr:Length))
-countsCol <- colnames(counts)
-rownames(md) <- countsCol
-
-# check to make sure columns and rows match for metadata and count data
-all(rownames(md) %in% colnames(counts))
-all(rownames(md) == colnames(counts)) # if false run code below
-counts <- counts[, rownames(md)]
-#write.csv(counts, "counts.csv", row.names = T)
-
-## emapper annotations
-#annoRaw <- read.delim("emapper.emapper.annotations",
-  #                    skip = 4,
-   #                   header = TRUE)
-# remove last 3 rows
-#tail(annoRaw)
-#anno <- annoRaw[-( (nrow(annoRaw) - 2) : nrow(annoRaw) ), ] 
-#anno$gene_id <- str_extract(anno[[1]], "^MSTRG\\.\\d+")
-#tail(annoRaw)
-
-# remove duplicates by keeping best hit (highest bit-score)
-#annoBest <- anno %>%
-#  group_by(gene_id) %>%
- # slice_max(order_by = score, with_ties = FALSE) 
-
-#rownames(annoBest) <- annoBest$gene_id
-
-## DESeq on full dataset, then just look at MSTRG.5605
-dds <- DESeqDataSetFromMatrix(countData = counts,
-                              colData = md,
-                              design = ~tissue)
-# prefilter
-smallestGroupsize <- 8
-keep <- rowSums(counts(dds) >= 10) >= smallestGroupsize
-dds <- dds[keep, ]
-
-# run DESeq
-dds <- DESeq(dds)
-resultsNames(dds)
-
-# full model
-res <- results(dds, name = "tissue_spleen_vs_skin")
-res <- res[order(res$padj),]
-summary(res)
-
-# extract DEGs
-genes <- as.data.frame(res)
-genes$gene_id <- rownames(genes)
-degs <- genes %>% 
-  filter(log2FoldChange > 0.5 | log2FoldChange < -0.5) %>% 
-  filter(padj < 0.1)
-
-sxph <- genes %>% filter(gene_id == "MSTRG.5605")
-vol1 <- EnhancedVolcano(genes,
-                         lab = "",
-                         x = "log2FoldChange",
-                         y = "padj",
-                         pCutoff = 0.001,
-                         FCcutoff = 1,
-                         pointSize = 1.0,
-                         labSize = 2.0,
-                         title = "",
-                         subtitle = "")
-
-vol1
